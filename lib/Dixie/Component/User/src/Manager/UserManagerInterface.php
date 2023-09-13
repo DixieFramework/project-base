5f5d3b035fe796698d246d68ea40bb18d604a7e4
subdi0x <subdi0x@protonmail.com>
2023-09-13 18:41:09 +0000

    Initial Commit

---
+++ lib/Dixie/Component/User/src/Manager/UserManagerInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talav\Component\User\Manager;
+
+use Talav\Component\Resource\Manager\ManagerInterface;
+use Talav\Component\User\Model\UserInterface;
+
+/**
+ * Interface to be implemented by user managers. This adds a level
+ * of abstraction between your application, and the actual repository.
+ *
+ * All changes to users should happen through this interface.
+ */
+interface UserManagerInterface extends ManagerInterface
+{
+    /**
+     * Find a user by its username.
+     */
+    public function findUserByUsername(string $username): ?UserInterface;
+
+    /**
+     * Finds a user by its email.
+     */
+    public function findUserByEmail(string $email): ?UserInterface;
+
+    /**
+     * Finds a user by its username or email.
+     */
+    public function findUserByUsernameOrEmail(string $usernameOrEmail): ?UserInterface;
+
+    /**
+     * Updates the canonical username and email fields for a user.
+     */
+    public function updateCanonicalFields(UserInterface $user): void;
+}
---
+++ lib/Dixie/Component/User/src/Model/UpdatedBy.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talav\Component\User\Model;
+
+trait UpdatedBy
+{
+    protected ?UserInterface $updatedBy = null;
+
+    public function getUpdatedBy(): ?UserInterface
+    {
+        return $this->updatedBy;
+    }
+
+    public function setUpdatedBy(?UserInterface $updatedBy): void
+    {
+        $this->updatedBy = $updatedBy;
+    }
+}
