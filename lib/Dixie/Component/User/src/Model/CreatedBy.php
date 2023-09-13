33d7db6fa00ec6dfbf897d7d7b7dd78afba925ad
subdi0x <subdi0x@protonmail.com>
2023-09-13 19:56:52 +0000

    Initial Commit

---
+++ lib/Dixie/Component/User/src/Model/CreatedBy.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talav\Component\User\Model;
+
+trait CreatedBy
+{
+    protected ?UserInterface $createdBy = null;
+
+    public function getCreatedBy(): ?UserInterface
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?UserInterface $createdBy): void
+    {
+        $this->createdBy = $createdBy;
+    }
+}
