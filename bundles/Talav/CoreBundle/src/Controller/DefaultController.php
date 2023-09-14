82c6d96379d2476699c2fa69b0d531286539d0c1
subdi0x <subdi0x@protonmail.com>
2023-09-14 04:17:36 +0200

    CoreBundle

--- bundles/Talav/CoreBundle/src/Controller/DefaultController.php
+++ bundles/Talav/CoreBundle/src/Controller/DefaultController.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Talav\CoreBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Talav\CoreBundle\Controller\AbstractController;
 
-class DefaultController extends Abstractontroller
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/talav_core")
--- config/bundles.php
+++ config/bundles.php
@@ -26,4 +26,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Talav\BundleGeneratorBundle\TalavBundleGeneratorBundle::class => ['all' => true],
+    Talav\CoreBundle\TalavCoreBundle::class => ['all' => true],
 ];
