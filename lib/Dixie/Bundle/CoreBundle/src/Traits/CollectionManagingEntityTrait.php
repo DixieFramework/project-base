6fc606c1e6b4e434f6d5ffa23e24b42916ec5f4e
mads <phpnull@hotmail.com>
2023-09-16 00:12:13 +0200

    Collection entity trait

---
+++ lib/Dixie/Bundle/CoreBundle/src/Traits/CollectionManagingEntityTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talav\CoreBundle\Traits;
+
+trait CollectionManagingEntityTrait
+{
+	private function addCollectionElement(string $property, mixed $item, ?string $itemOwnershipProperty = null): self
+	{
+		if (!$this->$property->contains($item)) {
+			$this->$property[] = $item;
+
+			if ($itemOwnershipProperty) {
+				$setter = $this->getCollectionElementSetter($itemOwnershipProperty);
+				$item->$setter($this);
+			}
+		}
+
+		return $this;
+	}
+
+	private function removeCollectionElement(string $property, mixed $item, ?string $itemOwnershipProperty = null): self
+	{
+		if ($this->$property->removeElement($item)) {
+			if ($itemOwnershipProperty) {
+				$getter = $this->getCollectionElementGetter($itemOwnershipProperty);
+				$setter = $this->getCollectionElementSetter($itemOwnershipProperty);
+				// set the owning side to null (unless already changed)
+				if ($item->$getter() === $this) {
+					$item->$setter(null);
+				}
+			}
+		}
+
+		return $this;
+	}
+
+	private function getCollectionElementGetter(string $property): string
+	{
+		return 'get'.ucfirst($property);
+	}
+
+	private function getCollectionElementSetter(string $property): string
+	{
+		return 'set'.ucfirst($property);
+	}
+}
