fbb4da5786ef32df8e5fd780ce8e97722272f740
mads <phpnull@hotmail.com>
2023-10-03 07:07:28 +0200

    many updates

--- lib/Dixie/Bundle/UserBundle/src/EventListener/UserControllerSubscriber.php
+++ lib/Dixie/Bundle/UserBundle/src/EventListener/UserControllerSubscriber.php
@@ -38,7 +38,7 @@ class UserControllerSubscriber implements EventSubscriberInterface
     }
 
     public function registerCurrentController(ControllerEvent $event): void
-    {dd($event);
+    {
         // this check is needed because in Symfony a request can perform any
         // number of sub-requests. See
         // https://symfony.com/doc/current/components/http_kernel.html#sub-requests
